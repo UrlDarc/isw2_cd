44dde706b3584d1cbe6658eee8ae161979ec09d1
UrlDarc <darodriguezc@correo.url.edu.gt>
2026-05-18 09:41:36 -0600

    Rompiendo pruebas

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
     res.json({ 
-        message: 'Carnet 1164XXX',
+        message: 'mensaje roto',
         version: '1.0.0'
     });
 });
